fd060a9610cfd3f897914d5094b0fb7baec30ece
Ben Ferreira <44374622+FerreiraBen@users.noreply.github.com>
2021-02-09 17:05:05 -0400

    Complete code

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ const short int FALSE = 0;
     const char EMPTY = '-';
     const char IA = 'X';
     const char HUMAN = 'O';
-    const char VELHA = 'V';
+    const char DRAW = 'V';
 
 typedef struct board{
     char board[3][3];
-    char jogadorAtual;
+    char currentPlayer;
 } Tboard;
 
 Tboard *createBoard(){
@@ -26,147 +26,140 @@ Tboard *createBoard(){
             board->board[i][j] = EMPTY;
     }
 
-    board->jogadorAtual = HUMAN;
+    board->currentPlayer = HUMAN;
 
     return board;
 }
 
 
 
-int iguais3(char a, char b, char c){
-    if(a==b && b==c && a!=EMPTY)
-        return 1;
-    return 0;
+int triEquals(char a, char b, char c){
+    return (a==b && b==c && a!=EMPTY) ? 1 : 0;
 }
 
 int max(int a, int b){
-    if(a>b)
-        return a;
-    return b;
+    return (a>b) ? a: b;
 }
 int min(int a, int b){
-    if(a<b)
-        return a;
-    return b;
+    return (a<b) ? a : b;
 }
 
 
-char verificaGanhador(Tboard *board){
-    char ganhador = EMPTY;
+char verifyWinner(Tboard *board){
+    char winner = EMPTY;
     int i,j;
 
-    // Ganhador na Horizontal
+    // winner horizontally
     for(i=0; i<3; i++){
-        if(iguais3(
+        if(triEquals(
             board->board[0][i],
             board->board[1][i],
             board->board[2][i]
             ))
-            ganhador = board->board[0][i];
+            winner = board->board[0][i];
     }
 
-
-    // Ganhador na Vertical
+    // winner vertically
      for(i=0; i<3; i++){
-        if(iguais3(
+        if(triEquals(
             board->board[i][0],
             board->board[i][1],
             board->board[i][2]
             ))
-            ganhador = board->board[i][0];
+            winner = board->board[i][0];
     }
 
-    // Ganhador na Diagonal
-    if(iguais3(
+    // winner diagonally
+    if(triEquals(
         board->board[0][0],
         board->board[1][1],
         board->board[2][2]
         )){
-        ganhador = board->board[0][0] ;
+        winner = board->board[0][0] ;
     }
-    if(iguais3(
+    if(triEquals(
         board->board[0][2],
         board->board[1][1],
         board->board[2][0]
         )){
-        ganhador = board->board[0][2] ;
+        winner = board->board[0][2] ;
     }
 
-    int espacos = 0;
+    int spaces = 0;
 
     for(int i = 0; i < 3; i++) {
       for(int j = 0; j < 3; j++) {
         if(board->board[i][j] == EMPTY) {
-          espacos++;
+          spaces++;
         }
       }
     }
 
-    if(ganhador==EMPTY && espacos == 0)
-        return VELHA;
-    return ganhador;
+    if(winner==EMPTY && spaces == 0)
+        return DRAW;
+    return winner;
 
 }
 
 int scores(char result){
-    if(result == IA)
-        return 10;
-    else if(result == HUMAN)
-        return -10;
-    else if(result == VELHA)
-        return 0;
+    switch(result){
+        case IA: return 10;
+        case HUMAN: return -10;
+        case DRAW: return 0;
+        default: return 0;
+    }
 }
 
 
-int minimax(Tboard *board, int profundidade, int maximizando){
-    char resultado = verificaGanhador(board);
-    if(resultado!=EMPTY)
-        return scores(resultado);
+int minimax(Tboard *board, int deepth, int maximizing){
+    char result = verifyWinner(board);
+    if(result!=EMPTY)
+        return scores(result);
 
-    if(maximizando){
-       int melhorPonto = -INF;
+    if(maximizing){
+       int bestPoint = -INF;
        for(int i=0; i<3; i++){
            for(int j=0; j<3; j++){
                if(board->board[i][j] == EMPTY){
                    board->board[i][j] = IA;
-                   int ponto = minimax(board, profundidade + 1, FALSE);
+                   int point = minimax(board, deepth + 1, FALSE);
                    board->board[i][j] = EMPTY;
-                   melhorPonto = max(ponto, melhorPonto);
+                   bestPoint = max(point, bestPoint);
                }
            }
        }
-        return melhorPonto;
+        return bestPoint;
     }
 
     else{
-        int melhorPonto = INF;
+        int bestPoint = INF;
         for(int i=0; i<3; i++){
            for(int j=0; j<3; j++){
                if(board->board[i][j] == EMPTY){
                    board->board[i][j] = HUMAN;
-                   int ponto = minimax(board, profundidade + 1, TRUE);
+                   int point = minimax(board, deepth + 1, TRUE);
                    board->board[i][j] = EMPTY;
-                   melhorPonto = min(ponto, melhorPonto);
+                   bestPoint = min(point, bestPoint);
                }
            }
        }
-        return melhorPonto;
+        return bestPoint;
     }
 }
 
 
-void melhorMovimento(Tboard *board){
-    int melhorPonto = -INF;
-    int x,y,ponto;
+void bestMove(Tboard *board){
+    int bestPoint = -INF;
+    int x,y,point;
 
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             if(board->board[i][j] == EMPTY){
                 board->board[i][j] = IA;
-                ponto = minimax(board, 0, FALSE);
+                point = minimax(board, 0, FALSE);
                 board->board[i][j] = EMPTY;
-                if(ponto>melhorPonto){
-                    melhorPonto = ponto;
+                if(point>bestPoint){
+                    bestPoint = point;
                     x = i;
                     y = j;
                 }
@@ -175,11 +168,11 @@ void melhorMovimento(Tboard *board){
     }
 
     board->board[x][y] = IA;
-    board->jogadorAtual = HUMAN;
+    board->currentPlayer = HUMAN;
 
 }
 
-void imprimirBoard(Tboard *board){
+void printBoard(Tboard *board){
     for(int i=0; i<3; i++){
         if(i!=2)
             printf("\t\t_%c_|_%c_|_%c_\n", board->board[i][0], board->board[i][1], board->board[i][2]);
@@ -215,10 +208,8 @@ void intro(){
 }
 
 
-short int movimentoValido(Tboard *board, int i, int j){
-    if(board->board[i][j] == EMPTY)
-        return 1;
-    return 0;
+short int validMove(Tboard *board, int i, int j){
+    return (board->board[i][j] == EMPTY) ? 1 : 0;
 }
 
 
@@ -227,74 +218,74 @@ int main(){
 
     int x,y,f;
 
-    Tboard *quadro;
+    Tboard *board;
     short int loopGame = TRUE;
     short int loopAll = TRUE;
 
 
     while(loopAll){
-        quadro = createBoard();
+        board = createBoard();
         system("cls");
-        printf("Voce gostaria de comecar primeiro? (0/1)\n");
+        printf("Would you like to start with the first move?? (0/1)\n");
         short int start;
-        scanf("%d", &start);
+        scanf("%hd", &start);
         while(start != 0 && start != 1){
             system("cls");
-            printf("Invalido. Responda com 0 (para nao) ou 1 (para sim).\n");
-            printf("Voce gostaria de comecar primeiro? (0/1)\n");
-            scanf("%d", &start);
+            printf("Invalid answer. Press 0 for No, or 1 for Yes..\n");
+            printf("Would you like to start with the first move?? (0/1)\n");
+            scanf("%hd", &start);
         }
         system("cls");
         if(!start){
-            quadro->jogadorAtual = IA;
-            melhorMovimento(quadro);
+            board->currentPlayer = IA;
+            bestMove(board);
         }
 
         while(loopGame){
-            char comoTa = verificaGanhador(quadro);
+            char comoTa = verifyWinner(board);
             if(comoTa!=EMPTY){
                 printf("\n");
-                imprimirBoard(quadro);
+                printBoard(board);
                 if(comoTa == HUMAN)
-                    printf("\nPARABENS, VOCE VENCEU!\n");
+                    printf("\nCongradulations!! You won!\n");
                 else if(comoTa == IA)
-                    printf("\nPERDEU, OTARIO!\n");
-                else if(comoTa == VELHA)
-                    printf("\nVELHOU, OTARIO!\n");
+                    printf("\nOh, No! You lose.\n");
+                else if(comoTa == DRAW)
+                    printf("\nDraw, babe.\n");
 
-                printf("Gostaria de jogar de novo? (0/1)\n");
+                printf("Would you like to play again? (0/1)\n");
                 scanf("%d",&f);
                 while(f != 0 && f != 1){
                     system("cls");
-                    printf("Invalido. Responda com 0 (para nao) ou 1 (para sim).\n");
-                    printf("Gostaria de jogar de novo? (0/1)\n");
+                    printf("Invalid answer. Press 0 for No, or 1 for Yes..\n");
+                    printf("Would you like to play again? (0/1)\n");
                     scanf("%d",&f);
                 }
                 if(f==1){
-                    quadro = createBoard();
+                    board = createBoard();
                     break;
                 }
                 else if(f==0){
-                    printf("\n\t\t Voce acabou de usar um programa de CAVALCANTE, J.B.C.\n\t\t\tObrigado!\n\n");
+                    printf("\n\t\t You just used a program made by CAVALCANTE, Josias B. F.\n\t\t\tThank you!\n\n");
                     system("pause");
                     return 0;
                 }
             }
 
-            else if(quadro->jogadorAtual == HUMAN){
+            else if(board->currentPlayer == HUMAN){
                 printf("\n");
-                imprimirBoard(quadro);
-                printf("\n\nDigite a linha e a coluna: (ex: 2 3)\n");
+                printBoard(board);
+                printf("\n\nType the line and column (exemple: 2 3):\n");
                 scanf("%d %d", &x, &y);
-                while(!movimentoValido(quadro, x-1, y-1)){
-                    printf("Coordenada invalida. Tente novamente.\n");
+                while(!validMove(board, x-1, y-1)){
+                    printf("Invalid coordinate. Try again.\n");
                     scanf("%d %d", &x, &y);
                 }
-                quadro->board[x-1][y-1] = HUMAN;
-                quadro->jogadorAtual = IA;
+                board->board[x-1][y-1] = HUMAN;
+                board->currentPlayer = IA;
 
-                if(comoTa = verificaGanhador(quadro) != VELHA)
-                    melhorMovimento(quadro);
+                if(comoTa == verifyWinner(board) != DRAW)
+                    bestMove(board);
             }
 
             system("cls");
@@ -304,3 +295,5 @@ int main(){
 
     return 0;
 }
+
+  
